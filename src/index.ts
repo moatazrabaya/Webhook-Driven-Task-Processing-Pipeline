925b3bf95e811bbf96be7094e231ac75edb6de97
moatazrabaya <mutazrabaya203@gmail.com>
2026-03-24 06:45:45 +0200

    update index.ts, use error middleware

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,12 @@
 import express from "express";
+import { errorMiddleWare } from "./api/middleware.js";
 
 const app = express();
 const PORT = 8080;
 
 app.use(express.json());
+
+app.use(errorMiddleWare);
 
 app.listen(PORT, () => {
   console.log(`Server is running at http://localhost:${PORT}`);
