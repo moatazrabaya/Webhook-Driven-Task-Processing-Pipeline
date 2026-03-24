4b2a234024fcbde4e1a08858af92d0b1ee92dbb7
moatazrabaya <mutazrabaya203@gmail.com>
2026-03-24 07:15:37 +0200

    update drizzle.config.ts, get the database url from config.ts

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,10 +1,11 @@
 import { defineConfig } from "drizzle-kit";
+import { config } from "./src/config";
 
 export default defineConfig({
   schema: "./src/db/schema.ts",
   out: "./src/db/migrations",
   dialect: "postgresql",
   dbCredentials: {
-    url: "postgres://postgres:postgres@localhost:5432/task_pipeline_db",
+    url: config.db.url,
   },
 });
