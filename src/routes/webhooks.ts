2ac901e54b3e7c485229c5ac064aa8f7cda91325
moatazrabaya <mutazrabaya203@gmail.com>
2026-03-24 09:32:18 +0200

    rename src/handlers/webhooks.ts to src/handlers/webhookHandler.ts

--- src/routes/webhooks.ts
+++ src/routes/webhooks.ts
@@ -1,5 +1,5 @@
 import express from "express";
-import { handlerWebhook } from "../handlers/webhooks.js";
+import { handlerWebhook } from "../handlers/webhookHandler.js";
 
 const router = express.Router()
 
